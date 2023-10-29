5a99f7fc40078fc97b44d82c171f05b7f8f5c2e1
Patrick James McEwen <pmcewen@myth61.stanford.edu>
2023-10-29 15:18:07 -0700

    scan

--- scan/scan.cu
+++ scan/scan.cu
@@ -44,12 +44,12 @@ static inline int nextPow2(int n) {
 // places it in result
 
 __global__ void upsweep_kernel(int* result, int N, int two_dplus1, int two_d) {
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int index = two_dplus1 * (blockIdx.x * blockDim.x + threadIdx.x);
     result[index+two_dplus1-1] += result[index+two_d-1];
 }
 
 __global__ void downsweep_kernel(int* result, int N, int two_dplus1, int two_d) {
-    int index = blockIdx.x * blockDim.x + threadIdx.x;
+    int index = two_dplus1 * (blockIdx.x * blockDim.x + threadIdx.x);
     int t = result[index + two_dplus1 - 1];
     result[index + two_d - 1] = result[index + two_dplus1 - 1];
     result[index + two_dplus1 - 1] += t;
