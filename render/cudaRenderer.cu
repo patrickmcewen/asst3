356ad5ac30fb6ad54f1606a350e32dd5f8e39d0c
Patrick James McEwen <pmcewen@myth58.stanford.edu>
2023-10-31 15:24:34 -0700

    render

--- render/cudaRenderer.cu
+++ render/cudaRenderer.cu
@@ -886,7 +886,7 @@ CudaRenderer::render() {
     for (int x = 0; x < params.gridDim_x; x++) {
         for (int y = 0; y < params.gridDim_y; y++) {
             int total_pairs_offset = (y * params.gridDim_x) + x;
-            int circles_per_block_offset = (cuConstRendererParams.size_of_one_row * y) + (cuConstRendererParams.size_of_one_block * x);
+            int circles_per_block_offset = (params.size_of_one_row * y) + (params.size_of_one_block * x);
             int* circles_per_block_start = circles_per_block_final + circles_per_block_offset;
             kernelRenderPixels<<<gridDimCircles, blockDimCircles>>>(circles_per_block_start, total_pairs + total_pairs_offset, x, y);
         }
