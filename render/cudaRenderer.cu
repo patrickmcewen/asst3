59939eacf56b9d62218f4b71650347e5c179325f
Patrick James McEwen <pmcewen@myth55.stanford.edu>
2023-10-31 01:26:51 -0700

    render

--- render/cudaRenderer.cu
+++ render/cudaRenderer.cu
@@ -531,7 +531,7 @@ __global__ void kernelBoundCircles() {
             float boxB = y * (cuConstRendererParams.blockDim_y) + cuConstRendererParams.blockDim_y - 1;
             printf("top: %f, bottom: %f, left: %f, right: %f, p.x: %f, p.y: %f, rad: %f\n", boxT, boxB, boxL, boxR, p.x, p.y, rad);
             printf("accessing %d index vs size of circles_per_block: %d\n", circles_per_block_index, cuConstRendererParams.numCircles * cuConstRendererParams.gridDim_x * cuConstRendererParams.gridDim_y);
-            printf("image width: %f, image height: %f\n", cuConstRendererParams.imageWidth, cuConstRendererParams.imageHeight);
+            printf("image width: %d, image height: %d\n", cuConstRendererParams.imageWidth, cuConstRendererParams.imageHeight);
             circles_per_block[circles_per_block_index] = circleInBox(p.x, p.y, rad, boxL, boxR, boxT, boxB);
             printf("result was %d\n", circles_per_block[circles_per_block_index]);
         }
