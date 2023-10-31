b68db477b90dceac00576d36c21ca5530c63a5b0
Patrick James McEwen <pmcewen@myth56.stanford.edu>
2023-10-30 21:50:23 -0700

    render

--- render/cudaRenderer.cu
+++ render/cudaRenderer.cu
@@ -673,8 +673,8 @@ CudaRenderer::render() {
     cudaDeviceSynchronize();*/
 
     dim3 blockDim(16, 16);
-    GlobalConstants hostConstRendererParams = (GlobalConstants*)malloc(sizeof(GlobalConstants)); 
-    cudaMemcpy(hostConstRendererParams, cuConstRendererParams, sizeof(GlobalConstants), cudaMemcpyDeviceToHost);
+    GlobalConstants* hostConstRendererParams = (GlobalConstants*)malloc(sizeof(GlobalConstants)); 
+    cudaMemcpy(*hostConstRendererParams, cuConstRendererParams, sizeof(GlobalConstants), cudaMemcpyDeviceToHost);
     dim3 gridDim((hostConstRendererParams->imageWidth + blockDim.x - 1) / blockDim.x, (hostConstRendererParams->imageHeight + blockDim.y - 1) / blockDim.y);
     printf("imageWidth: %d, height: %d\n", hostConstRendererParams->imageWidth, hostConstRendererParams->imageHeight);
     printf("grid dims are x- %d and y- %d\n", gridDim.x, gridDim.y);
