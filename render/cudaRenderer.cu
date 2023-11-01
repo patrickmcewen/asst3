6ea9c82862cadfabdd13d09ee8af0c9847292db5
Patrick James McEwen <pmcewen@myth66.stanford.edu>
2023-10-31 22:37:41 -0700

    render

--- render/cudaRenderer.cu
+++ render/cudaRenderer.cu
@@ -490,7 +490,7 @@ __global__ void kernelRenderPixels(int* circles_per_block_final, int* total_pair
     int* circles_per_block_start = circles_per_block_final + circles_per_block_offset;
     int total_pairs_val = *(total_pairs + total_pairs_offset);
     bool check_pixel = false;
-    if (x == 389 && y == 205) {
+    if (x == 491 && y == 205) {
         printf("circle numbers: %d\n", total_pairs_val);
         check_pixel = true;
     }
@@ -831,7 +831,7 @@ CudaRenderer::render() {
     int* print_data_bound = (int*)malloc(sizeof(int) * params.pow2Circles * params.gridDim_x * params.gridDim_y);
     cudaMemcpy(print_data_bound, circles_per_block, sizeof(int) * params.pow2Circles * params.gridDim_x * params.gridDim_y, cudaMemcpyDeviceToHost);
 
-    int xx = 389 / 16;
+    int xx = 491 / 16;
     int yy = 205 / 16;
     int circles_per_block_offset = (params.size_of_one_row * yy) + (params.size_of_one_block * xx);
     int* circles_per_block_start = print_data_bound + circles_per_block_offset;
@@ -926,8 +926,8 @@ CudaRenderer::render() {
 
     float* image_data_print = (float*)malloc(sizeof(float) * params.imageWidth * params.imageHeight * 4);
     cudaMemcpy(image_data_print, cudaDeviceImageData, sizeof(float) * params.imageWidth * params.imageHeight * 4, cudaMemcpyDeviceToHost);
-    float4* data = (float4*)(&image_data_print[4 * (205 * params.imageWidth + 389)]);
-    printf("y = 205 (row), x = 389 (column): %f, %f, %f\n", data->x, data->y, data->z);
+    float4* data = (float4*)(&image_data_print[4 * (205 * params.imageWidth + 491)]);
+    printf("y = 205 (row), x = 491 (column): %f, %f, %f\n", data->x, data->y, data->z);
     cudaDeviceSynchronize();
 }
 
