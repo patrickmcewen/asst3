51be546a854546faa77e2c5df4f601ee7d24484e
patrickmcewen <pmcewen@stanford.edu>
2023-11-05 22:20:04 -0800

    render

--- render/cudaRenderer.cu
+++ render/cudaRenderer.cu
@@ -66,8 +66,8 @@ __constant__ float  cuConstNoise1DValueTable[256];
 #define COLOR_MAP_SIZE 5
 __constant__ float  cuConstColorRamp[COLOR_MAP_SIZE][3];
 
-#define XX 500
-#define YY 500
+#define XX 389
+#define YY 205
 
 // including parts of the CUDA code from external files to keep this
 // file simpler and to seperate code that should not be modified
