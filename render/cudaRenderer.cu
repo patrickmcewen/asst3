11f7c1b9774f80c834dbb1420733dbc8e4a895d8
patrickmcewen <pmcewen@stanford.edu>
2023-11-05 23:39:56 -0800

    render

--- render/cudaRenderer.cu
+++ render/cudaRenderer.cu
@@ -551,10 +551,10 @@ __global__ void kernelBoundCircles(int* circles_per_block) {
     int* circles_per_block_start = circles_per_block + (cuConstRendererParams.size_of_one_row * blockIdx.y) + (cuConstRendererParams.size_of_one_block * blockIdx.x); 
 
     int thread_ind = threadIdx.y * blockDim.x + threadIdx.x;
+    printf("thread_ind: %d\n", circle_ind);
 
     for (int i = 0; i < cuConstRendererParams.numCircles; i+= BLOCKSIZE) {
         int circle_ind = i + thread_ind;
-        printf("circle_ind: %d\n", circle_ind);
         int index3 = 3 * circle_ind;
         // read position and radius
         //printf("getting p and rad\n");
