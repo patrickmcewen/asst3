71cda11ce023fcc6d5218fa1ef3c130828456d5f
patrickmcewen <pmcewen@stanford.edu>
2023-11-07 10:10:28 -0800

    render

--- render/cudaRenderer.cu
+++ render/cudaRenderer.cu
@@ -456,7 +456,7 @@ __global__ void kernelSharedMem() {
 
     int offset = 0;
     // loop over all circles. BLOCKSIZE - 1 because exclusive scan can't capture the last element.
-    for (int i = 0; i < cuConstRendererParams.numCircles; i+= BLOCKSIZE-1) {
+    for (int i = 0; i < cuConstRendererParams.numCircles; i+= BLOCKSIZE) {
         /* if (thread_idx == 0 && x == 0 && y == 0) {
             printf("i: %d, numcircles: %d\n", i, cuConstRendererParams.numCircles);
         } */
@@ -486,13 +486,10 @@ __global__ void kernelSharedMem() {
         //printf("thread_index: %d, result: %d\n", thread_idx, circles[thread_idx]);
 
         // get correct circle indices and total number of circles
-        if (thread_idx < BLOCKSIZE-1) {
-            if (circles[thread_idx] == 1) {
-                circleInds[circles_scanned[thread_idx]] = thread_idx + offset;
-            }
-        } else {
-            numCircles = circles_scanned[BLOCKSIZE-1];
+        if (circles[thread_idx]) {
+            circleInds[circles_scanned[thread_idx]] = thread_idx + offset;
         }
+        numCircles = circles_scanned[BLOCKSIZE-1];
         __syncthreads();
         /* if (thread_idx == 0 && x == 0 && y == 0) {
             printf("done with index gathering\n");
