f66bfbeae0e52ee5c6082ee013d2bb025f37b135
Young Chen <youngch@myth58.stanford.edu>
2023-11-08 20:51:20 -0800

    fix last minute scan

--- scan/scan.cu
+++ scan/scan.cu
@@ -80,13 +80,13 @@ void exclusive_scan(int* input, int N, int* result)
     // scan.
     N = nextPow2(N);
     int arrSize = sizeof(float) * N;
+    int two_dplus1 = 2 * two_d;
+    int numThreads = N / two_dplus1;
     dim3 numBlocks((numThreads + THREADS_PER_BLOCK - 1) / THREADS_PER_BLOCK);
     dim3 threadsPerBlock((numThreads + numBlocks.x - 1) / numBlocks.x);
 
     cudaMemcpy(result, input, arrSize, cudaMemcpyDeviceToDevice);
     for (int two_d = 1; two_d <= N/2; two_d *= 2) {
-        int two_dplus1 = 2 * two_d;
-        int numThreads = N / two_dplus1;
         upsweep_kernel<<<numBlocks, threadsPerBlock>>>(result, N, two_dplus1, two_d);
         cudaDeviceSynchronize();
     }
@@ -95,8 +95,6 @@ void exclusive_scan(int* input, int N, int* result)
     cudaDeviceSynchronize();
 
     for (int two_d = N/2; two_d >= 1; two_d /= 2) {
-        int two_dplus1 = 2 * two_d;
-        int numThreads = N / two_dplus1;
         downsweep_kernel<<<numBlocks, threadsPerBlock>>>(result, N, two_dplus1, two_d);
         cudaDeviceSynchronize();
     }
